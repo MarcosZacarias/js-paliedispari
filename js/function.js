94d3a345fb8ba44475fb9bf0c79f477113de3bb0
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-07-27 16:57:06 +0200

    js add: funcion word palindrome

--- js/function.js
+++ js/function.js
@@ -1,25 +1,53 @@
-// Generiamo un numero casuale da 1 a 5
-// creo un valore = "numero casuale"
-function numberRandomGenerator() {
-  const numeroCasuale = Math.floor(Math.random() * (5 - 1)) + 1;
-  return numeroCasuale;
-}
+// ! Pari e Dispari
 
-// SE
-function risultatoVincitore() {
-  let winner;
-  if (
-    // "scelta utente" = "pari" e "somma" è pari (somma divisibile per 2)
-    (sceltaUtente === sceltaPari && somma % 2 == 0) || // O
-    // "scelta utente" = "dispari" e "somma" è dispari (somma non divisibile per 2)
-    (sceltaUtente === sceltaDispari && !(somma % 2 == 0))
-  ) {
-    // Stampo "il vincitore è l'utente"
-    winner = "Il vincitore è l'utente";
-  } else {
-    // ALTRIMENTI
-    // Stampo "il vincitore è il computer"
-    winner = "Il Vincitore è il computer";
+// // Generiamo un numero casuale da 1 a 5
+// // creo un valore = "numero casuale"
+// function numberRandomGenerator() {
+//   const numeroCasuale = Math.floor(Math.random() * (5 - 1)) + 1;
+//   return numeroCasuale;
+// }
+
+// // SE
+// function risultatoVincitore() {
+//   let winner;
+//   if (
+//     // "scelta utente" = "pari" e "somma" è pari (somma divisibile per 2)
+//     (sceltaUtente === sceltaPari && somma % 2 == 0) || // O
+//     // "scelta utente" = "dispari" e "somma" è dispari (somma non divisibile per 2)
+//     (sceltaUtente === sceltaDispari && !(somma % 2 == 0))
+//   ) {
+//     // Stampo "il vincitore è l'utente"
+//     winner = "Il vincitore è l'utente";
+//   } else {
+//     // ALTRIMENTI
+//     // Stampo "il vincitore è il computer"
+//     winner = "Il Vincitore è il computer";
+//   }
+//   return winner;
+// }
+
+// ! Palidroma
+
+// Creo un valore con le lettere della "_parola utente_" ma al contrario
+function parolaPalindroma() {
+  let parolaAlContario = "";
+  for (let i = parolaUtente.length - 1; i > -1; i--) {
+    const lettera = parolaUtente[i];
+    console.log("lettera: " + lettera);
+
+    parolaAlContario += lettera;
   }
-  return winner;
+
+  console.log("Parola scritta dall'utente al contrario: " + parolaAlContario);
+
+  // **SE** la "_parola utente_" **é** = "_parola utente al contrario_"
+  if (parolaAlContario == parolaUtente) {
+    // - Stampo: "la parola è palindroma"
+    parolaAlContario = "La parola scritta è palindroma";
+  } else {
+    // **ALTRIMENTI**
+    // - Stampo: "la parola non è palindroma"
+    parolaAlContario = "La parola scritta non è palindroma";
+  }
+  return parolaAlContario;
 }
--- js/main.js
+++ js/main.js
@@ -1,48 +1,62 @@
-// Chiedo a l'utente di scegliere tra pari o dispari
-// creo un valore "scelta utente"
-let sceltaUtente = prompt("Inserisci PARI o DISPARI");
-sceltaUtente = sceltaUtente.toLowerCase();
-console.log("Scelta dell'utente: " + sceltaUtente);
+// ! Pari e Dispari
 
-// creo un valore "pari"
-let sceltaPari = "pari";
-console.log(sceltaPari);
+// // Chiedo a l'utente di scegliere tra pari o dispari
+// // creo un valore "scelta utente"
+// let sceltaUtente = prompt("Inserisci PARI o DISPARI");
+// sceltaUtente = sceltaUtente.toLowerCase();
+// console.log("Scelta dell'utente: " + sceltaUtente);
 
-// creo un valore "dispari"
-let sceltaDispari = "dispari";
-console.log(sceltaDispari);
+// // creo un valore "pari"
+// let sceltaPari = "pari";
+// console.log(sceltaPari);
 
-let sceltaSbagliata = "Dati non validi, inserisci la parola PARI o DISPARI";
+// // creo un valore "dispari"
+// let sceltaDispari = "dispari";
+// console.log(sceltaDispari);
 
-// TODO COSA FARE SE MI SCRIVE ALTRE PAROLE
-while (sceltaUtente != sceltaPari && sceltaUtente != sceltaDispari) {
-  alert(sceltaSbagliata);
-  sceltaUtente = prompt("Inserisci PARI o DISPARI");
-}
+// let sceltaSbagliata = "Dati non validi, inserisci la parola PARI o DISPARI";
 
-console.log("Scelta dell'utente: " + sceltaUtente);
+// // TODO COSA FARE SE MI SCRIVE ALTRE PAROLE
+// while (sceltaUtente != sceltaPari && sceltaUtente != sceltaDispari) {
+//   alert(sceltaSbagliata);
+//   sceltaUtente = prompt("Inserisci PARI o DISPARI");
+// }
 
-// Chiediamo a l'utente di inserire un numero da 1 a 5
-// creo un valore = "numero utente"
-let numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
-console.log("Numero dell'utente: " + numeroUtente);
+// console.log("Scelta dell'utente: " + sceltaUtente);
 
-let numeroSbagliato = "Dati non validi, inserisci un numero da 1 a 5";
+// // Chiediamo a l'utente di inserire un numero da 1 a 5
+// // creo un valore = "numero utente"
+// let numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
+// console.log("Numero dell'utente: " + numeroUtente);
 
-// TODO COSA FARE SE MI SCRIVE UN VALORE CHE NON SIA TRA 1 E 5
-while (numeroUtente > 5 || numeroUtente < 1) {
-  alert(numeroSbagliato);
-  numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
-}
-console.log("Numero dell'utente: " + numeroUtente);
+// let numeroSbagliato = "Dati non validi, inserisci un numero da 1 a 5";
 
-const numeroCasuale = numberRandomGenerator();
-console.log("Numero casuale Generato: " + numeroCasuale);
+// // TODO COSA FARE SE MI SCRIVE UN VALORE CHE NON SIA TRA 1 E 5
+// while (numeroUtente > 5 || numeroUtente < 1) {
+//   alert(numeroSbagliato);
+//   numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"));
+// }
+// console.log("Numero dell'utente: " + numeroUtente);
 
-// Sommo i due numeri
-const somma = numeroUtente + numeroCasuale;
-console.log("Somma dei numeri: " + somma);
+// const numeroCasuale = numberRandomGenerator();
+// console.log("Numero casuale Generato: " + numeroCasuale);
 
-// Stampo il vincitore
-const winner = risultatoVincitore();
-console.log(winner);
+// // Sommo i due numeri
+// const somma = numeroUtente + numeroCasuale;
+// console.log("Somma dei numeri: " + somma);
+
+// // Stampo il vincitore
+// const winner = risultatoVincitore();
+// console.log(winner);
+
+// ! Palidroma
+
+// Chiedo all'utente di scrivere una parola
+const parolaUtente = prompt("Inserisci una parola");
+console.log("Parola scritta dall'utente: " + parolaUtente);
+
+console.log(parolaUtente.length);
+
+// Stampo se la parola è palindroma
+let parolaAlContario = parolaPalindroma();
+console.log(parolaAlContario);
